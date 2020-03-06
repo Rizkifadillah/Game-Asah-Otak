13c59e02a242ab3649827049e641dedef45c31c0
RizkiFadillah <rizkifadillah26.rf@gmail.com>
2020-03-06 10:23:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:game_asah_otak/src/app.dart';
+
+void main() => runApp(MyApp());
+
---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:game_asah_otak/src/page/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tebak Angka",
+      home: Home(),
+    );
+  }
+}
